8e998e24c3693232205d10f31f94f9cfe6065a9e
Francisco Javier <144556857+Franbor2003@users.noreply.github.com>
2023-12-19 00:29:42 +0100

    Create Circunferencia.java
    Código que mediante el radio de una circunferencia, calcula la longitud y el área de la circunferencia

---
+++ Circunferencia.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class ejercicio4 {
+        public static void main(String[] args) {
+                
+     Scanner entrada = new Scanner(System.in);
+        System.out.println("Escribe el radio; ");
+        Double radio = entrada.nextDouble();
+
+
+        Double Longitud = 2*3.14*radio;
+        Double Area = 3.14*radio*radio;
+
+        System.out.println("la lngitud de la circunferencia es: " + Longitud);
+        System.out.println("El area de la circunferencia es: " + Area);
+        
+        entrada.close();
+        }
+}
